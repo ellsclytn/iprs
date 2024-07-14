bf55cf7b092c03f01066863e6dcbc303c0aa7ab4
Damien Kingsley <actuallydamo@gmail.com>
2024-07-14 18:05:39 +1000

    Parse IP nets
    Co-authored-by: Ellis Clayton <ellis@ellis.codes>

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,7 @@
 use clap::Parser;
-
+use ipnet::{IpNet, Ipv4Net, Ipv6Net};
+use std::str::FromStr;
+use std::process;
 
 #[derive(Parser)]
 struct Cli {
@@ -8,6 +10,16 @@ struct Cli {
 
 fn main() {
     let args = Cli::parse();
+    let ip_input = &args.ip;
 
-    println!("Value for ip: {}", args.ip);
+    let ip = if let Ok(ipv4) = Ipv4Net::from_str(ip_input) {
+        IpNet::V4(ipv4)
+    } else if let Ok(ipv6) = Ipv6Net::from_str(ip_input) {
+        IpNet::V6(ipv6)
+    } else {
+        eprintln!("Invalid IP address");
+        process::exit(1);
+    };
+
+    println!("ip is: {}", ip);
 }
